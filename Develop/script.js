85de2a3ee3b5bf5b30a551d2616b75f0475390bc
Kristian Conner <Kristianconner17@gmail.com>
2021-08-20 21:42:44 -0400

    updated JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var special = "!#$%&()*+<_>?:;<=>?@[]|^_'{\}~";
 
 function generatePassword() {
-  
+  var password = "";
+  var length = 8;
+  var upperCase = confirm("Would you like uppercase characters included?");
+  var lowerCase = confirm("Would you like lowercase characters included?");
+  var specialChar = confirm("Would you like special characters included?");
+  var numbers = confirm("Would you like numbers included?");
+
+  if (upperCase) {
+    var random = Math
+  }
+
 }
 
 // Write password to the #password input
